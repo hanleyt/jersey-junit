dc28fb5bf9a0a06c57c0394dd1be1557185a27d1
Tomas Hanley <tomas.hanley@murex.com>
2018-02-10 20:09:43 +0000

    Add jersey extension, as well as gradle and travis config.

---
+++ src/main/java/JerseyExtension.java
@@ -0,0 +1,59 @@
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import java.util.function.Supplier;
+
+public class JerseyExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
+
+    private final Supplier<Application> applicationSupplier;
+    private JerseyTest jerseyTest;
+
+    public JerseyExtension(Supplier<Application> applicationSupplier) {
+        this.applicationSupplier = applicationSupplier;
+    }
+
+    private void initJerseyTest() {
+        jerseyTest = new JerseyTest() {
+            @Override
+            protected Application configure() {
+                return applicationSupplier.get();
+            }
+        };
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        jerseyTest.tearDown();
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        initJerseyTest();
+        jerseyTest.setUp();
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return parameterContext.getParameter().getType().equals(Client.class) || parameterContext.getParameter().getType().equals(WebTarget.class);
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        Class<?> parameterType = parameterContext.getParameter().getType();
+
+        if (parameterType.equals(Client.class)) {
+            return jerseyTest.client();
+        }
+        if (parameterType.equals(WebTarget.class)) {
+            return jerseyTest.target();
+        }
+        throw new IllegalStateException("Unrecognised parameter type: " + parameterType);
+    }
+}
---
+++ src/test/java/JerseyExtensionIT.java
@@ -0,0 +1,22 @@
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import javax.ws.rs.client.WebTarget;
+
+import static com.google.common.truth.Truth.assertThat;
+
+class JerseyExtensionIT {
+
+    @RegisterExtension
+    JerseyExtension jerseyExtension = new JerseyExtension(this::configureJersey);
+
+    private ResourceConfig configureJersey() {
+        return new ResourceConfig();
+    }
+
+    @Test
+    void parameters_are_injected(WebTarget target) {
+        assertThat(target.getUri()).isNotNull();
+    }
+}
